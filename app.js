4f4b22f89e725a2a4c683ea0bc3134f82dc32acd
zuber Ab <zuber1077@yahoo.com>
2018-02-04 16:57:17 +0800

    Updating the Budget_ UI Controller

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ var budgetController = (function () {
       this.value = value;
     };
 
+    var calculateTotal = function(type){
+        var sum = 0;
+        data.allItems[type].forEach(function(cur){
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+        /*  
+        0
+        [200, 400, 100]
+        sum = 0 + 200
+        sum = 200 + 400
+        sum = 600 + 700
+        */
+    };
+
 
     var data = {
         allItems: {
@@ -21,7 +36,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -47,6 +64,37 @@ var budgetController = (function () {
 
             //return the new element
             return newItem;
+        },
+        calculateBudget: function(){
+
+            //calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            //calculate the budget: income - expreses
+            data.budget = data.totals.inc - data.totals.exp;
+            //calcualte the paecentages of income taht we spent
+            if(data.totals.inc > 0){
+                data.percentage = Math.round(data.totals.exp / data.totals.inc * 100);
+            }else{
+                data.percentage = -1;
+            }
+
+
+            //Expense = 100 income 300, spent 33.333% = 100/300 = 0.3333 * 100 
+        },
+
+        getBudget: function(){
+            return{
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
+        },
+
+        testing: function(){
+            console.log(data)
         }
     };
 
@@ -64,7 +112,11 @@ var UIcontroller = (function () {
          inputValue:'.add__value',
          inputButton: '.add__btn',
          incomeContainer: '.income__list',
-         expensesContainer: '.expenses__list'
+         expensesContainer: '.expenses__list',
+         budgetLabel: '.budget__value',
+         incomeLabel: '.budget__income--value',
+         expensesLabel: '.budget__expenses--value',
+         percentageLabel: '.budget__expenses--percentage'
         };
 
         return {
@@ -112,7 +164,22 @@ var UIcontroller = (function () {
 
             },
 
-            getDOMstrings: function() {
+            displayBudget: function(obj){
+
+                document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+                document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+                document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+                if(obj.percentage > 0){
+                    document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+                }else{
+                    document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+
+                }
+
+            },
+
+             getDOMstrings: function() { 
                 return DOMstrings;
             }
         };
@@ -139,11 +206,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     var updateBudget = function(){
 
         //1.calculate the budget
-
+        budgetCtrl.calculateBudget();
         //2.return the budget
-
-        //5.display the budget on the UI
-
+        var budget = budgetCtrl.getBudget();
+        //3 .display the budget on the UI
+        UICtrl.displayBudget(budget);
     };
 
     var ctrlAddItem = function() {
@@ -168,6 +235,12 @@ var controller = (function(budgetCtrl, UICtrl) {
         return{
             init: function(){
                 console.log('app has Ster');
+                UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: 0
+            });
                 setupEventListener();
             }
         };
